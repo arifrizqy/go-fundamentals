2bda51ac2391f1f6d1c4ab2a894d1f20ea30b3d0
arifrizqy <m.arifrizqy.f@gmail.com>
2025-01-28 20:37:57 +0700

    learn: boolean data type

---
+++ src/booleantype.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Tipe data boolean adalah tipe data yang paling sederhana pada semua bahasa pemrograman.
+	// Karena tipe data boolean hanya memiliki dua nilai.
+	// Tipe data boolean direpresentasikan dengan kata kunci `bool`.
+	// | Nilai Boolean | Keterangan |
+	// true | benar
+	// false | salah
+
+	fmt.Println("[Boolean/bool] benar:", true)
+	fmt.Println("[Boolean/bool] salah:", false)
+}
